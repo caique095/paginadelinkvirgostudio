624df2a11082e37fb77a84cc8aa15787e0af99d4
Virgo Studio <virgostudio@gmail.com>
2025-01-29 19:42:38 -0300

    Primeiro commit

---
+++ js/animations.js
@@ -0,0 +1,62 @@
+// Função para carregar a animação do WhatsApp
+var animationWhatsapp = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-whatsapp'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'whatsapp-animation.json' // Caminho para o arquivo JSON da animação do WhatsApp
+});
+
+// Função para carregar a animação do E-book
+var animationEbook = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-ebook'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'ebook-animation.json' // Caminho para o arquivo JSON da animação do E-book
+});
+
+// Função para carregar a animação do Instagram
+var animationInstagram = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-instagram'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'instagram-animation.json' // Caminho para o arquivo JSON da animação do Instagram
+});
+
+// Função para carregar a animação do Site
+var animationSite = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-site'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'site-animation.json' // Caminho para o arquivo JSON da animação do Site
+});
+
+// Função para carregar a animação do Behance (para as redes sociais)
+var animationBehance = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-behance'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'behance-animation.json' // Caminho para o arquivo JSON da animação do Behance
+});
+
+// Função para carregar a animação do Facebook (se aplicável)
+var animationFacebook = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-facebook'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'facebook-animation.json' // Caminho para o arquivo JSON da animação do Facebook
+});
+
+// Função para carregar a animação do Formulário
+var animationForm = lottie.loadAnimation({
+    container: document.getElementById('lottie-container-form'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'form-animation.json' // Caminho para o arquivo JSON da animação do Formulário
+});
